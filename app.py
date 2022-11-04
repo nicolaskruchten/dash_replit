f9001738ef0e10f89bfac88c549d769130c23718
Nicolas Kruchten <nicolas@kruchten.com>
2022-11-04 12:29:27 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from dash import Dash, html, dcc, Input, Output
+
+app = Dash(__name__)
+#server = app.server
+app.layout = html.Div(children=[dcc.Input(id="in"), html.H1(id="out")])
+
+
+@app.callback(Output('out', 'children'), Input('in', 'value'))
+def cb(input_value):
+  return f"Hello {input_value or ''}"
+
+
+app.run_server(host='0.0.0.0', debug=True)
